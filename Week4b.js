39747734d805272b16e5c64aa4482d131c29fbef
robert <rwwhite@email.neit.edu>
2023-10-24 10:07:23 -0400

    Week4a

---
+++ Week4b.js
@@ -0,0 +1,16 @@
+let age = 18;
+age = age + 1;
+console.log("You are now " + age + " years old.");
+
+age++;  // short-hand notation
+console.log("You are now " + age + " years old.");
+
+let ammo =6;
+ammo = ammo - 1;    // long-hand
+ammo--;             // short-hand
+console.log("Ammo left in chamber: " + ammo);
+
+let balance = 0;
+balance = balance + 4.18;   // price of milk
+balance = balance - 0.50;//discount due to coupon
+console.log("You pay: " + balance);
