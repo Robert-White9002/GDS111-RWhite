78d3e7bf0e502d5e46e4e2107acd7a2245f3446a
robert <rwwhite@email.neit.edu>
2023-10-24 10:08:26 -0400

    Week4a

--- Week4a.js
+++ Week4a.js
@@ -1,5 +1,5 @@
 var wideScope =7;
-if (wideScope == 7) {
+if (wideScope == 7) { 
     let localScope = 10;
     console.log("localScope inside if-block = " +localScope)
     console.log("wideScope inside if-block = " + wideScope);
