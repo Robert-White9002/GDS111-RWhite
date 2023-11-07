79bc7b16982956de24df91e09067dce4d0dd315d
robert <rwwhite@email.neit.edu>
2023-11-07 10:33:56 -0500

    Week 6

---
+++ Week6a.js
@@ -0,0 +1,19 @@
+const playerArr = ["Mario", "Luigi", "Peach"]; // IDs
+const pwdArr = ["diamond", "emerald", "gold"]; // passwords
+
+let playerID = prompt ("Enter Player ID: ");
+let password = prompt ("Type password: ");
+
+let validUser = false; // a boolean
+for(let i=0; i < playerArr.length; i++){ // note .length
+    if (playerID == playerArr[i] &&
+        password == pwdArr[i]) {
+            validUser = true; // flip the boolean
+        }
+    }
+
+    if (validUser == true) {
+        console.log("Welcome to the game!");
+    } else {
+        console.log("Go away hacker ...");
+    }
