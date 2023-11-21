6abe0eb763a2f700b7068743d8362f64d044ced9
robert <rwwhite@email.neit.edu>
2023-11-21 15:03:09 -0500

    Week7c

--- Week7c.js
+++ Week7c.js
@@ -3,9 +3,40 @@ class Warrior {
     constructor() {
         this.health = 0;
         this.damage = 0;
-
     }
 
     //public behaviors
-    
+    setHealth(newHealth) {
+        this.health = newHealth;
+    }
+    getHealth() {
+        return this.health;
+    }
+    setDamage(newDamage) {
+        this.damage = newDamage;
+    }
+    getDamage() {
+        return this.damage;
+    }
 }
+
+const leviathan = new Warrior();
+leviathan.setDamage(10);
+leviathan.setHealth(100);
+console.log("leviathan's Stats:");
+console.log(leviathan.getDamage());
+console.log(leviathan.getHealth());
+
+const behemoth = new Warrior();
+behemoth.setDamage(15);
+behemoth.setHealth(90);
+console.log("behemoth's Stats:");
+console.log(behemoth.getDamage());
+console.log(behemoth.getHealth());
+
+const thanos = new Warrior();
+thanos.setDamage(99);
+thanos.setHealth(100);
+console.log("thanos's Stats:");
+console.log(thanos.getDamage());
+console.log(thanos.getHealth());
